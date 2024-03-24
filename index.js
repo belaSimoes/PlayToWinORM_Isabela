016312c7ce91e4592d90ab73e470049370b5802d
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2024-03-23 21:28:45 -0400

    ajuste

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require ("dotenv").config();
 const conn = require("./db/conn");
 
-conn.authenticate().then(() => {
-    console.log("conectado ao  banco de dados com sucesso!");
+conn
+    .authenticate()
+    .then(() => {
+      console.log("conectado ao  banco de dados com sucesso!");
 })
-     .catch( (err)=>{
-        console.log("Ocorreu um erro: " + err)
+    .catch( (err)=>{
+       console.log("Ocorreu um erro: " + err)
      } );
